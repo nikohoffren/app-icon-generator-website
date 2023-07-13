6938d786fda0b0a69ebc5e40b3d05d949f99ea79
Niko Hoffren <niko.hoffren@gmail.com>
2023-07-13 15:01:27 +0300

    Modify FileUpload.jsx Tailwind classes

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
-import { generateIcon } from '../utils/GenerateIcon';
+import React, { useState } from "react";
+import { generateIcon } from "../utils/GenerateIcon";
 
 const FileUpload = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
-  const onFileChange = event => {
-    setSelectedFile(event.target.files[0]);
-  };
+    const onFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
 
-  const onFileUpload = () => {
-    if (!selectedFile) {
-      return;
-    }
-    //* Generate icons of a few different sizes as an example
-    [48, 72, 96, 144, 192, 512].forEach(size => generateIcon(selectedFile, size));
-  };
+    const onFileUpload = () => {
+        if (!selectedFile) {
+            return;
+        }
+        //* Generate icons of a few different sizes as an example
+        [48, 72, 96, 144, 192, 512].forEach((size) =>
+            generateIcon(selectedFile, size)
+        );
+    };
 
-  return (
-    <div>
-      <input type="file" onChange={onFileChange} />
-      <button onClick={onFileUpload}>Upload</button>
-    </div>
-  );
+    return (
+        <div>
+            <input type="file" onChange={onFileChange} />
+            <button
+                onClick={onFileUpload}
+                className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-1 px-4 rounded"
+            >
+                Upload
+            </button>
+        </div>
+    );
 };
 
 export default FileUpload;
-
